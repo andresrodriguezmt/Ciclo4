722f8d50ca095dd34751498f5a34428aa2147421
andresrodriguezmt <andres.rodriguez1.mt@correo.usa.edu.co>
2021-12-07 16:51:26 -0500

    borrar

--- Reto2/src/main/java/com/ciclo4/reto2/repository/crud/GadgetCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ciclo4.reto2.repository.crud;
-
-import com.ciclo4.reto2.model.Gadget;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface GadgetCrudRepository extends MongoRepository<Gadget, Integer>{
-    
-}
